86a7dfaca2d503c6d8b4fb013f7936a2f67b8af0
frepingod <frepingod@gmail.com>
2021-12-21 22:28:48 +0300

    card class + rename project in pom + spring 2.6.1 version

--- src/main/java/ru/netology/model/Card.java
+++ src/main/java/ru/netology/model/Card.java
@@ -3,13 +3,28 @@ package ru.netology.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Data
 @AllArgsConstructor
 public class Card {
 
-    private String cardFromNumber;
-    private String cardFromValidTill;
-    private String cardFromCVV;
-    private String cardToNumber;
+    @NotBlank(message = "number must not be null")
+    @Size(min = 19, max = 19)
+    @Pattern(regexp = "[0-9]{4}[ ][0-9]{4}[ ][0-9]{4}[ ][0-9]{4}")
+    private String number;
+
+    @NotBlank(message = "valid till must not be null")
+    @Size(min = 5, max = 5)
+    @Pattern(regexp = "(0[1-9]|1[0-2])[/][0-9]{2}")
+    private String validTill;
+
+    @NotBlank(message = "cvv must not be null")
+    @Size(min = 3, max = 3)
+    @Pattern(regexp = "[0-9]{3}")
+    private String cvv;
+
     private Amount amount;
 }
