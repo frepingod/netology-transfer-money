8e064e7cc27ecdfefc176b64ee05400e7c81c1c7
frepingod <frepingod@gmail.com>
2021-12-25 15:56:01 +0300

    added controller

---
+++ src/main/java/ru/netology/controller/TransferMoneyController.java
@@ -0,0 +1,27 @@
+package ru.netology.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import ru.netology.model.request.TransferRQ;
+import ru.netology.model.request.ConfirmRQ;
+import ru.netology.model.response.TransferAndConfirmRS;
+import ru.netology.service.TransferMoneyService;
+
+@RestController
+@AllArgsConstructor
+public class TransferMoneyController {
+
+    private TransferMoneyService service;
+
+    @PostMapping("/transfer")
+    public TransferAndConfirmRS postTransfer(@RequestBody TransferRQ transferRequest) {
+        return service.postTransfer(transferRequest);
+    }
+
+    @PostMapping("/confirmOperation")
+    public TransferAndConfirmRS postConfirmOperation(@RequestBody ConfirmRQ confirmRequest) {
+        return service.postConfirmOperation(confirmRequest);
+    }
+}
