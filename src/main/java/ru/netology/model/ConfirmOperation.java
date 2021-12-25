6a5d0895696d623c29385a9ef04894f8902bc8f1
frepingod <frepingod@gmail.com>
2021-12-25 15:53:55 +0300

    added model ConfirmOperation

---
+++ src/main/java/ru/netology/model/ConfirmOperation.java
@@ -0,0 +1,22 @@
+package ru.netology.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Data
+@AllArgsConstructor
+public class ConfirmOperation {
+
+    @NotBlank(message = "operation id must not be null")
+    @Pattern(regexp = "^[0-9]*$")
+    private String operationId;
+
+    @NotBlank(message = "code must not be null")
+    private String code;
+
+    @NotBlank(message = "description must not be null")
+    private String description;
+}
